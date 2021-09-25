8bd7e83946497fad529dad715e7f1a893225d561
paweltatarczak <78876457+paweltatarczak@users.noreply.github.com>
2021-09-25 18:36:28 +0200

    Update docker.cmd

--- docker.cmd
+++ docker.cmd
@@ -2,18 +2,19 @@
 @cd /d "%~dp0"
 
 @rem Download and install VirtualBox
-curl -o ./VirtualBox.exe https://download.virtualbox.org/virtualbox/6.1.26/VirtualBox-6.1.26-145957-Win.exe
+curl -o ./VirtualBox.exe https://download.virtualbox.org/virtualbox/6.1.26/VirtualBox-6.1.26-145957-Win.exe --ssl-no-revoke
 VirtualBox.exe --extract --path Vbox --silent
 call msiexec /i Vbox\VirtualBox-6.1.26-r145957.msi /passive /norestart
 del VirtualBox.exe
 rmdir /S /Q Vbox
 
 @rem Download and install Vagrant
-curl -o ./vagrant.msi https://releases.hashicorp.com/vagrant/2.2.18/vagrant_2.2.18_x86_64.msi
+curl -o ./vagrant.msi https://releases.hashicorp.com/vagrant/2.2.18/vagrant_2.2.18_x86_64.msi --ssl-no-revoke
 call msiexec /i vagrant.msi /passive /norestart
 del vagrant.msi
 
 @rem Prepare sync folder
 mkdir data
 
+echo VirtualBox and Vagrant has been installed.
 pause
